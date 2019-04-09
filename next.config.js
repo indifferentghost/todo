a130fbf032d04dcc9cab19da8bba4bbeb2a3d5a5
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-04-08 22:32:58 -0400

    init

---
+++ next.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+  target: 'serverless',
+  module: {
+    loaders: [
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
+      }
+    ]
+  }
+}
