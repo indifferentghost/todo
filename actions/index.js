95719cb48baac0be1245e6109db14d6361a6301e
Thomas Dillard <thomas.dillard.studios@gmail.com>
2019-04-10 22:44:06 -0400

    Fixed error with nested arrays

---
+++ actions/index.js
@@ -0,0 +1,6 @@
+import { ADD_TODO, addTodo } from "./todoActions";
+
+export default {
+  ADD_TODO,
+  addTodo,
+};
--- reducers/todoReducer.js
+++ reducers/todoReducer.js
@@ -1,8 +1,9 @@
 import { ADD_TODO } from '../actions/todoActions';
 
 export const todoReducer = (state, { type, data }) => {
+  console.log('reducer', data)
   switch (type) {
     case ADD_TODO:
-      return [...state, data];
+      return [...state, ...data];
   }
 }
